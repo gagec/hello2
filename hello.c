46c37d7e8cda940a5e55ba5c23711a5c12675283
gagecrowell <gcrowell3@huskers.unl.edu>
2020-08-18 18:29:57 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Gage Crowell
+ * Date: 2020/18/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Gage Crowell! \n");
+
+  return 0;
+}
